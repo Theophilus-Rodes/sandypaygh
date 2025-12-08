4fc4e872109397e0f9e2a33532c8ef294e9f431b
Theophilus-Rodes <edutheo33@gmail.com>
2025-12-08 14:35:48 +0000

    Add files via upload

--- backend/shortcode/ussd.js
+++ backend/shortcode/ussd.js
@@ -57,7 +57,7 @@ if (!DB_PASSWORD) {
 }
 
 // Your short code extension (from Moolre)
-const EXTENSION_EXPECTED = "888";
+const EXTENSION_EXPECTED = "717";
 
 // ✅ Moolre config (from your account)
 const MOOLRE = {
